7d0ac60e6e538d06fbe68dd48f1fd1b81ce3af82
BinhMisfit <binh@misfitwearables.com>
2013-11-08 17:36:42 +0700

    Update bashscripts

--- files/default/checking_resque_workers.sh
+++ files/default/checking_resque_workers.sh
@@ -4,15 +4,18 @@
 
 sleep 15 # Delay a little bit after running chef-client (Maybe resque workers has not started immediately!)
 
+echo "Checking..."
 key_word="var/www/apps/shineapi/releases/"
 line=`ps -Ao command | grep $key_word`
-
+echo $line
 
 id=${line:37:40}
 echo "The current commit ID that resque workers are using:"
 echo $id
 
+echo "Checking..."
 line1=`ls -1 -t /var/www/apps/shineapi/releases`
+echo $line1
 echo "The current commit ID that the web-server is using:"
 id1=${line1:0:40}
 echo $id1
@@ -37,6 +40,7 @@ if [ "$id1" == "$id" ]
 then
   echo $MESSAGE1 | mail -s "$body1" "$back_end_email1" "$back_end_email2" "$back_end_email3" "$back_end_email4"
 else
+  echo "Resque workers have not been updated!!!"
   echo $MESSAGE2 | mail -s "$body2" "$back_end_email" "$back_end_email2" "$back_end_email3" "$back_end_email4"
   echo "DO restart resque workers!"
   echo $ACTION
--- metadata.rb
+++ metadata.rb
@@ -6,6 +6,4 @@ description      'Installs/Configures base'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
-description      "This repo will help anyone to install necessary plugins on Newrelic, such as, e.g, Memcached, AWS plugins,..."
-
 depends          "postfix"
